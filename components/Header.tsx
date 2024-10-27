70efb3461c5d8be8a809ead545c0f7c278a3f098
dflr10 <dafelor.10@hotmail.com>
2024-10-27 16:12:24 -0500

    feature(page,Header,Nav): created services page, add button and links header nav and pathname created

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">    
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Daniel Lozada <span className="text-accent">.</span>
           </h1>
         </Link>
+        {/*Nav menu desktop & hire me button */}
+        <div className="hidden xl:flex items-center gap-8"> 
+        <Nav/>
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div> 
+        {/*mobile Nav */}
+        <div className="xl:hidden">movile nav</div>       
       </div>
+      
     </header>
 
   )
